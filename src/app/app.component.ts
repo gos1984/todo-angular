05196053427018f975ed84edf7d584fbdfa6dc44
gos1984 <gos1984@list.ru>
2020-06-03 19:36:04 +0300

    add components and services category

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {Category} from './common/category';
+import {Priority} from './common/priority';
+import {Task} from './common/task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'todo-angular';
