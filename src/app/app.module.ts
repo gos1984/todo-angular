37adfc9721ef130973fd12a0ae4a0409c65d83d9
gos1984 <gos1984@list.ru>
2020-06-04 16:09:23 +0300

    Added components task and priority

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CategoryComponent } from './components/category/category.component';
+import { TaskComponent } from './components/task/task.component';
+import { PriorityComponent } from './components/priority/priority.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoryComponent
+    CategoryComponent,
+    TaskComponent,
+    PriorityComponent
   ],
   imports: [
     BrowserModule,
